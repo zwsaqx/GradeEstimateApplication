750a6db2d9ff3d8e5963ff460cfa7f5ea9fadbdf
Zachary Annabell <zwsaqx@gmail.com>
2023-10-20 13:56:10 +1300

    pages added

--- app/src/main/java/com/example/gradeestimatorapplication/MainActivity.kt
+++ app/src/main/java/com/example/gradeestimatorapplication/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.gradeestimatorapplication.ui.theme.GradeEstimatorApplicationTheme
-import androidx.compose.material3.Button
-import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.Alignment
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : ComponentActivity() {
@@ -22,13 +22,66 @@ class MainActivity : ComponentActivity() {
             GradeEstimatorApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier, color = MaterialTheme.colorScheme.background) {
-                    Greeting("Androids")
-                    Button("test")
-
+                    setContentView(R.layout.login_activity)
                 }
             }
         }
     }
+
+    fun loginButtonClick(view: View) {
+        val usernameEditText = findViewById<EditText>(R.id.editTextUsername)
+        val passwordEditText = findViewById<EditText>(R.id.editTextPassword)
+        val loginResultTextView = findViewById<TextView>(R.id.loginResultTextView)
+
+        val enteredUsername = usernameEditText.text.toString()
+        val enteredPassword = passwordEditText.text.toString()
+
+        val correctCredentials = mapOf(
+            "" to "",
+            "john" to "password",
+            // usernames and password list goes here
+        )
+
+        if (correctCredentials.containsKey(enteredUsername) && correctCredentials[enteredUsername] == enteredPassword) {
+            loginResultTextView.text = "Login Successful"
+            setContentView(R.layout.menu_activity)
+
+        } else {
+            loginResultTextView.text = "Login Failed"
+        }
+    }
+
+    fun forgotButtonClick(view: View) { setContentView(R.layout.forgot_password_activity)}
+    fun myPapersButtonClick(view: View) { setContentView(R.layout.my_papers_activity) }
+    fun menuButtonClick(view: View) { setContentView(R.layout.menu_activity) }
+    fun signupButtonClick(view: View) { setContentView(R.layout.register_activity) }
+    fun logoutButtonClick(view: View) { setContentView(R.layout.login_activity) }
+
+    fun refreshButtonClick(view: View) {
+        val updateMessage = "Refreshed Papers!"
+        Toast.makeText(applicationContext, updateMessage, Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.my_papers_activity) }
+
+    fun feedbackButtonClick(view: View) { setContentView(R.layout.feedback_activity) }
+
+    fun feedbackSubmitButtonClick(view: View) {
+        if (1 == 1) {   //will need to implement this later
+            val updateMessage = "Feedback Sent!"
+            Toast.makeText(applicationContext, updateMessage, Toast.LENGTH_SHORT).show()
+            //setContentView(R.layout.feedback_activity)
+
+        } else {
+            val updateMessage = "Feedback Failed!"
+            Toast.makeText(applicationContext, updateMessage, Toast.LENGTH_SHORT).show()
+        }
+        setContentView(R.layout.feedback_activity) }
+
+    fun aboutButtonClick(view: View) { setContentView(R.layout.about_activity) }
+
+    fun settingsButtonClick(view: View) { setContentView(R.layout.settings_activity) }
+
+
+
 }
 
 @Composable
@@ -37,31 +90,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "test $name!",
             modifier = modifier
     )
-}
 
-
-@Composable
-fun Button(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Button(
-            onClick = {
-            // Define the action to be performed when the button is clicked
-            }
-        ) {
-            Text(
-                text = "$name"
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GradeEstimatorApplicationTheme {
-        Button("tst")
-    }
 }
